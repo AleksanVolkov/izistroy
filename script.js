f171b31e7ca67f14e12180fa3fe6cf7ee8194dd2
Alexandr <kodak333vas@gmail.com>
2024-10-13 01:42:10 +0300

    13.10/2

--- script.js
+++ script.js
@@ -18,3 +18,63 @@ openModal.addEventListener('click',()=>{
     body.style.overflow="hidden"
     
 })
+
+
+
+/*slider...........*/
+
+const arrowRight = document.querySelectorAll('.arrow_right'),
+      arrowLeft = document.querySelectorAll('.arrow_left'),
+      galaryField= document.querySelector('.galary_field'),
+      galery= document.querySelector('.galery');
+
+
+      galery.addEventListener('click',(e)=>{
+        
+        const target= e.target;
+        console.dir(target.className)
+
+        if(target.tagName=='DIV' && target.className=="arrow_right" ){
+            console.log('click')
+            for(let i=0;i<=galaryField.length;i++){
+                console.log('for')
+            }
+            galaryField.style.transform= 'translateX(-325px)';
+            galaryField.style.transition= 'all 0.8s';
+        }
+         else if(target.tagName=='DIV' && target.className=="arrow_left" ){
+            galaryField.style.transform= 'translateX(0)';
+            galaryField.style.transition= 'all 0.8s';
+        }
+      })
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    //   arrowRight.forEach((item)=>{
+    //     item.addEventListener('click',(e)=>{
+    //         const target= e.target;
+    //         if(target==item){
+    //             console.log('click')
+    //             galaryField.style.transform= 'translateX(-350px)';
+    //             galaryField.style.transition= 'all 0.8s';
+    //         }
+    //     })
+    //   })
+
+      
+    //   arrowLeft.forEach((item)=>{
+    //     item.addEventListener('click',(e)=>{
+    //         const target= e.target;
+    //         if(target==item){
+    //             galaryField.style.transform= 'translateX(-350px)';
+    //             galaryField.style.transition= 'all 0.8s';
+    //         }
+    //     })
+    //   })
