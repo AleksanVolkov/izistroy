7b13c416e3251bf4d9ffc1abad6dedc3a8cdaa05
Alexandr <kodak333vas@gmail.com>
2024-10-14 00:12:29 +0300

    80%

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const body = document.querySelector('body'),
         closeModal = document.querySelector('.close'),
         modal=document.querySelector('.pop_up')
 
+const tabsParent= document.querySelector('.category'),
+    galeryArr= document.querySelectorAll('.galery'),
+    tabs=document.querySelectorAll('.tab_item');
 
 
 closeModal.addEventListener('click',()=>{
@@ -19,62 +22,32 @@ openModal.addEventListener('click',()=>{
     
 })
 
+/*portfolio***********************************/
+function showGalery(i=0){
+    galeryArr[i].style.display='flex'
+}
 
+function hideGalery(){
+    galeryArr.forEach(item=>{
+        item.style.display="none"
+    })
+}
 
-/*slider...........*/
+hideGalery();
+showGalery();
 
-const arrowRight = document.querySelectorAll('.arrow_right'),
-      arrowLeft = document.querySelectorAll('.arrow_left'),
-      galaryField= document.querySelector('.galary_field'),
-      galery= document.querySelector('.galery');
-
-
-      galery.addEventListener('click',(e)=>{
-        
-        const target= e.target;
-        console.dir(target.className)
-
-        if(target.tagName=='DIV' && target.className=="arrow_right" ){
-            console.log('click')
-            for(let i=0;i<=galaryField.length;i++){
-                console.log('for')
+tabsParent.addEventListener('click',(e)=>{
+    const target=e.target;
+    if(target && target.classList.contains('tab_item')){
+        tabs.forEach((item,i)=>{
+            if(target==item){
+                hideGalery()
+                showGalery(i);
             }
-            galaryField.style.transform= 'translateX(-325px)';
-            galaryField.style.transition= 'all 0.8s';
-        }
-         else if(target.tagName=='DIV' && target.className=="arrow_left" ){
-            galaryField.style.transform= 'translateX(0)';
-            galaryField.style.transition= 'all 0.8s';
-        }
-      })
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    //   arrowRight.forEach((item)=>{
-    //     item.addEventListener('click',(e)=>{
-    //         const target= e.target;
-    //         if(target==item){
-    //             console.log('click')
-    //             galaryField.style.transform= 'translateX(-350px)';
-    //             galaryField.style.transition= 'all 0.8s';
-    //         }
-    //     })
-    //   })
+        })
+    }
 
-      
-    //   arrowLeft.forEach((item)=>{
-    //     item.addEventListener('click',(e)=>{
-    //         const target= e.target;
-    //         if(target==item){
-    //             galaryField.style.transform= 'translateX(-350px)';
-    //             galaryField.style.transition= 'all 0.8s';
-    //         }
-    //     })
-    //   })
+})
+
+
+ 
