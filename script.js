42ff1ea48d08ceed774969f3d9a082e6c5b8b42e
Alexandr <kodak333vas@gmail.com>
2024-11-02 03:55:26 +0300

    final

--- script.js
+++ script.js
@@ -23,51 +23,79 @@ openModal.forEach(item=>{
 })
 
 /************************************************tg*/
-const allForm= document.querySelector('#popUpForm'),
-messageMod=document.querySelector('.message');
-      
+const allForm= document.querySelectorAll('#popUpForm'),
+dynamic=document.querySelector('.dynamic'),
+static=document.querySelector('.static'),
+messageMod=document.querySelector('.message'),
+messageMovStatic=document.querySelector('.message_static');      
+
 let TOKEN='7160931872:AAFX_4Ji0SmaLpaB4DHkWD1G3a6rauOO6GM';
 let URI_API=`https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 let CHAT_Id= '-1002480212657';
       
-
-  
-allForm.addEventListener('submit',function(e){
-    e.preventDefault()
-  
-
-
-   let message = '<b>Заявка с сайта</b>\n';
-   message+=`<b>имя:</b>${this.name.value}\n`;
-   message+=`<b>номер телефона:</b>${this.phone.value}`;
-
-  
-   
-    axios.post(URI_API, {
-       chat_id:CHAT_Id,
-       parse_mode:'html',
-       text:message
-       
-   })
-   .then((res)=>{
-     
+allForm.forEach(item=>{
+    item.addEventListener('submit',function(e){
+        e.preventDefault()
+      
     
-    this.name.value=''
-    this.phone.value='+375'
-    allForm.style.display="none"
-    messageMod.style.display="block"
+        let message = '<b>Заявка с сайта</b>\n';
+        message+=`<b>имя:</b>${this.name.value}\n`;
+        message+=`<b>номер телефона:</b>${this.phone.value}`;
+    
+        if(this.className=='dynamic'){
+            axios.post(URI_API, {
+                chat_id:CHAT_Id,
+                parse_mode:'html',
+                text:message
+                
+            })
+            .then((res)=>{
+            
+            
+            this.name.value=''
+            this.phone.value='+375'
+            dynamic.style.display="none"
+            messageMod.style.display="block"
+        
+            function resetMod(){
+                dynamic.style.display="flex"
+                messageMod.style.display="none"
+                setTimeout(closeMod(),500)
+        
+            }
+            setTimeout(resetMod,3500)
+            
+            })
+        
+        
+        }else{
+            axios.post(URI_API, {
+                chat_id:CHAT_Id,
+                parse_mode:'html',
+                text:message
+                
+            }).then((res)=>{
+            
+            
+                this.name.value=''
+                this.phone.value='+375'
+                static.style.display="none"
+                messageMovStatic.style.display="block"
+            
+                function resetMod(){
+                    static.style.display="flex"
+                    messageMovStatic.style.display="none"
+                    setTimeout(closeMod(),500)
+            
+                }
+                setTimeout(resetMod,3500)
+                
+                })
+        }
+     })
+})
+  
 
-    function resetMod(){
-        allForm.style.display="flex"
-        messageMod.style.display="none"
-        setTimeout(closeMod(),500)
-
-    }
-    setTimeout(resetMod,3500)
-   
-   })
-
- })
  //   .catch((error)=>{
 //     errorM.style.display="block";
 //     errorM.textContent =`собщение не отправленно!!!  код ошибки: ${error}`
